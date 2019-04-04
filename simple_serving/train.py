70a8586fa2837e0ca122888d6ee2864f8939643b
Byeongsoo Kim <ryujivan316@gmail.com>
2019-04-04 18:51:42 +0900

    no message

---
+++ simple_serving/train.py
@@ -0,0 +1,69 @@
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import requests
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import subprocess
+import tempfile
+import random
+import json
+import shutil
+
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+# scale the values to 0.0 to 1.0
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+# reshape for feeding into the model
+print('before reshape, train_images.shape : ', train_images.shape)
+print('before reshape, test_images.shape : ', test_images.shape)
+train_images = train_images.reshape(train_images.shape[0], 28, 28, 1)
+test_images = test_images.reshape(test_images.shape[0], 28, 28, 1)
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+print('\ntrain_images.shape: {}, of {}'.format(train_images.shape, train_images.dtype))
+print('test_images.shape: {}, of {}'.format(test_images.shape, test_images.dtype))
+
+model = keras.Sequential([
+  keras.layers.Conv2D(input_shape=(28,28,1), filters=8, kernel_size=3,
+                      strides=2, activation='relu', name='Conv1'),
+  keras.layers.Flatten(),
+  keras.layers.Dense(10, activation=tf.nn.softmax, name='Softmax')
+])
+model.summary()
+
+model.compile(optimizer=tf.train.AdamOptimizer(),
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+epochs = 5
+
+model.fit(train_images, train_labels, epochs=epochs)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print('\nTest accuracy: {}'.format(test_acc))
+
+MODEL_DIR = tempfile.gettempdir()
+version = 1
+export_path = os.path.join(MODEL_DIR, str(version))
+print('export_path = {}\n'.format(export_path))
+if os.path.isdir(export_path):
+  print('\nAlready saved a model, cleaning up\n')
+  shutil.retree(export_path)
+
+tf.saved_model.simple_save(
+    keras.backend.get_session(),
+    export_path,
+    inputs={'input_image': model.input},
+    outputs={t.name:t for t in model.outputs})
+
+print('\nSaved model:')
+                           
